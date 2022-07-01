f0dac1e13c96b318b7466b321ebca1f32e79b195
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-02 01:19:16 +0300

    Saat cinsinden hesaplama da eklendi

--- app/src/main/java/com/bcan/age_in_minutes/MainActivity.kt
+++ app/src/main/java/com/bcan/age_in_minutes/MainActivity.kt
@@ -3,7 +3,6 @@ package com.bcan.age_in_minutes
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private var _tvSelectedDate: TextView? = null
     private var _tvAgeInMinutes: TextView? = null
+    private var _tvAgeInHours: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,40 +21,42 @@ class MainActivity : AppCompatActivity() {
         val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
         _tvSelectedDate = findViewById(R.id.tvSelectedDate)
         _tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+        _tvAgeInHours = findViewById(R.id.tvAgeInHours)
         btnDatePicker.setOnClickListener {
-            _clickDatePicker()
+            clickDatePicker()
         }
 
 
     }
 
-    private fun _clickDatePicker() {
+    private fun clickDatePicker() {
         val myCalender = Calendar.getInstance()
         val year = myCalender.get(Calendar.YEAR)
         val month = myCalender.get(Calendar.MONTH)
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
-        val dpd = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDayofMonth ->
+        val dpd = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDayOfMonth ->
             Toast.makeText(this, "DatePickerBtn Pressed", Toast.LENGTH_LONG).show()
 
-            val selectedDate = "$selectedDayofMonth/${selectedMonth + 1}/$selectedYear"
+            val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
             _tvSelectedDate?.text = selectedDate
 
             val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
             val theDate = sdf.parse(selectedDate)
-                theDate?.let {
-                    val selectedDateInMinutes = theDate.time / 60000
+            theDate?.let {
+                val selectedDateInMinutes = theDate.time / 60000
+                val selectedDateInHours = theDate.time / 3600000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
 
-                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
-
-                   currentDate?.let {
-                       val currentDateInMinutes = currentDate.time / 60000
-
-                       val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
-
-                       _tvAgeInMinutes?.text = differenceInMinutes.toString()
-                   }
+                currentDate?.let {
+                    val currentDateInMinutes = currentDate.time / 60000
+                    val currentDateInHours = currentDate.time / 3600000
+                    val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                    val differenceInHours = currentDateInHours - selectedDateInHours
+                    _tvAgeInMinutes?.text = differenceInMinutes.toString()
+                    _tvAgeInHours?.text = differenceInHours.toString()
                 }
+            }
 
         }, year, month, day)
 
